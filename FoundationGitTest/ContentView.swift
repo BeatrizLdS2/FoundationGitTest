00cbef7a20fe8e28374220ff2dfd5cd4fc51996e
BeatrizLdS2 <beatriz@gmail.com>
2025-07-04 16:46:23 -0300

    Initial Commit

---
+++ FoundationGitTest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FoundationGitTest
+//
+//  Created by Beatriz Leonel on 04/07/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FoundationGitTest/FoundationGitTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoundationGitTestApp.swift
+//  FoundationGitTest
+//
+//  Created by Beatriz Leonel on 04/07/25.
+//
+
+import SwiftUI
+
+@main
+struct FoundationGitTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
