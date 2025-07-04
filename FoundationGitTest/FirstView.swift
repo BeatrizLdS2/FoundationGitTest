5eaee7f1461633051f23ff778c5d568ac7b0550d
BeatrizLdS2 <beatriz@gmail.com>
2025-07-04 17:09:01 -0300

    create first view

---
+++ FoundationGitTest/FirstView.swift
@@ -0,0 +1,20 @@
+//
+//  FirstView.swift
+//  FoundationGitTest
+//
+//  Created by Beatriz Leonel on 04/07/25.
+//
+
+import SwiftUI
+
+struct FirstView: View {
+    var body: some View {
+        VStack {
+            Text("First View")
+        }
+    }
+}
+
+#Preview {
+    FirstView()
+}
